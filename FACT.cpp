741d924c5790751748e9e161ae63782230b6a7d2
SHIVAM SHELKE <103065763+shvmshlk7@users.noreply.github.com>
2022-12-03 09:46:21 -0800

    Create FACT.cpp

---
+++ FACT.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int n;
+	    cin>>n;
+	    cpp_int fact=1;
+	    for(int i=;i>0;i--)
+	     fact=fact*i;
+	    cout<<fact<<endl; 
+	}
+	
+	return 0;
+}
+
+
+
+
